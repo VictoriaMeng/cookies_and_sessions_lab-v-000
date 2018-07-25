161c27693694632629f4d0b5abab8c21e70c2a5c
Victoria Meng <nights1ststar@protonmail.com>
2018-07-25 19:26:16 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def add
-    cart = cart
     cart << params[:product]
-    session[:cart] = cart
-    @cart = session[:cart]
-    binding.pry
     redirect_to root
   end
 end
